993ad69542262e2b92db5d01aa1472c05d2c5a70
Paulo-Luciana <Paulo-Luciana>
2017-01-18 15:32:12 -0200

    first commit

---
+++ AndroidStudioProjects/Agenda/app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,36 @@
+package br.com.alura.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import br.com.alura.agenda.R;
+import dao.AlunoDAO;
+
+/**
+ * Created by darks on 27/12/2016.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
+
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDAO dao = new AlunoDAO(context);
+        if (dao.ehAluno(telefone)) {
+            Toast.makeText(context, "Chegou um SMS de aluno!", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
+
+    }
+}
